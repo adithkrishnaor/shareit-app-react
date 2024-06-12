5a4088591891522b95724b0082b2dccb0515641b
adithkrishnaor <adithkrishnaor@gmail.com>
2024-06-12 23:37:58 +0530

    index page updated

---
+++ src/components/Post.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <div>
+        <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <center>
+                            <h3>Post Your Content</h3>
+                        </center>
+                        <div className="row g-3">
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Name</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Username</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Image Link</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Caption</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <center>
+                                    <button className="btn btn-info">Post</button>
+                                </center>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+  )
+}
+
+export default Post
